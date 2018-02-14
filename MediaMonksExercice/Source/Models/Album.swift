37f739a90a4b062c870486ad7c4270c0c7fbd598
Andre Vieira <vieira.agp@gmail.com>
2018-02-14 18:16:13 -0200

    creating the album model and photo model

---
+++ MediaMonksExercice/Source/Models/Album.swift
@@ -0,0 +1,22 @@
+//
+//  Album.swift
+//  MediaMonksExercice
+//
+//  Created by Andre Vieira on 14/02/18.
+//  Copyright © 2018 Andre Vieira. All rights reserved.
+//
+
+import Foundation
+import Mapper
+
+struct Album: Mappable {
+    var userId: Int
+    var albumId: Int
+    var title: String
+    
+    init(map: Mapper) throws {
+        try userId = map.from("userId")
+        try albumId = map.from("id")
+        try title = map.from("title")
+    }
+}
