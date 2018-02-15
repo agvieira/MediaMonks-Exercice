4810ee6c9297d2907c86b7dcf8407a954e79e235
Andre Vieira <vieira.agp@gmail.com>
2018-02-14 22:11:25 -0200

    create the ImageView extension and the protocol NibLoadable

--- MediaMonksExercice/Source/Modules/Photo/View/Cells/PhotoTableViewCell.swift
+++ MediaMonksExercice/Source/Modules/Photo/View/Cells/PhotoTableViewCell.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 protocol PhotoTableViewCellType {
     func setupCell(viewModel: PhotoCellViewModelType)
@@ -31,40 +30,5 @@ final class PhotoTableViewCell: UITableViewCell {
     
     private func setupPhoto() {
         self.photo.loadImage(url: self.viewModel?.photoUrl, placeholder: "ic_picture")
-    }
-}
-
-extension UIImageView {
-    
-    func loadImage(url: String = "", placeholder: String) {
-        if let imageUrl = URL(string: url) {
-            let resource = ImageResource(downloadURL: imageUrl, cacheKey: url)
-            self.kf.setImage(with: resource,
-                             placeholder: UIImage(named: placeholder),
-                             options: nil,
-                             progressBlock: nil,
-                             completionHandler: nil)
-        } else {
-            self.image = UIImage(named: placeholder)
-        }
-    }
-    
-    func loadImage(url: URL?, placeholder: String) {
-        if let imageUrl = url {
-            let resource = ImageResource(downloadURL: imageUrl, cacheKey: url?.absoluteString)
-            self.kf.setImage(with: resource,
-                             placeholder: UIImage(named: placeholder),
-                             options: nil,
-                             progressBlock: nil,
-                             completionHandler: nil)
-        } else {
-            self.image = UIImage(named: placeholder)
-        }
-    }
-    
-    func loadImage(url: String = "") {
-        guard let imageUrl = URL(string: url) else { return }
-        let resource = ImageResource(downloadURL: imageUrl, cacheKey: url)
-        self.kf.setImage(with: resource)
     }
 }
